5170914e084dbbbcd32aaed94aa80bbde3666784
mgtburid <50088674+mgtburid@users.noreply.github.com>
2020-06-20 00:56:47 -0400

    Create install.sh
    GPGIM installer.

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+distro=$(cat /etc/os-release)
+if [[ $distro == *"Red Hat"* || $distro == *"CentOs"*]]; then
+	echo -e "Apparently, you are running a Red Hat/CentOs box. The program will now need to utilize sudo privileges to do its job. Enter a password if prompted.\n"
+	sudo yum install gnupg2 openssl
+elif [[ $distro == *"Ubuntu"* ]]; then
+	echo -e "Apparently, you are running an Ubuntu box. The program will now need to utilize sudo privileges to do its job. Enter a password if prompted.\n"
+	sudo apt-get install gnupg2 openssl
+fi
+
+echo "Done."
