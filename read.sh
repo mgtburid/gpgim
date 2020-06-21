b001d45d61cdc82870d45c55654940df454ace4d
mgtburid <50088674+mgtburid@users.noreply.github.com>
2020-06-20 23:02:39 -0400

    Update read.sh

--- read.sh
+++ read.sh
@@ -1,8 +1,26 @@
 #!/bin/bash
 
 today=$(date +%Y-%m-%d)
-wget -m -np -c -R "index.html*" -P ./ ""
-[ ! -f message.pgp ] && echo "Error, nothing to read from (message.pgp is absent)." && exit 1
-gpg --decrypt message.pgp >& decrypted_messages
-cat decrypted_messages >> ${today}_messages_decrypted
-rm -f decrypted_messages
+filestack=$(pwd)/gpgim_files
+privkey=$(cat $filestack/.Rendezvous | cut -d " " -f1)
+rest=$(cat $filestack/.Rendezvous | cut -d " " -f2)
+
+[ -z $privkey ] && scp -v -r $rest. $filestack &> $filestack/.scp_output || scp -v -i $privkey $rest. $filestack &> $filestack/.scp_output
+[ ! -f message.gp ] && echo "No messages or message file is not called message.gp." && exit 1
+dir_con=$(ls $filestack -1 | grep .pub)
+dir_con_num=$(echo "$dir_con" | wc -l)
+case $1 in
+	--read-only|-ro)
+		gpg --decrypt message.gp &>> $filestack/${today}_decrypted_messages
+		shift
+		;;
+	*)	
+		gpg --decrypt message.gp &>> $filestack/${today}_decrypted_messages
+		for key in $(seq 1 $dir_con_num)
+		do
+			echo "Importing key '$key'." >> $filestack/.gpgim_keys
+			gpg --import $key &> $filestack/.gpgim_keys
+		done
+		shift
+		;;
+esac
