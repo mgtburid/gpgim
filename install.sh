0150f968fce3e9665dc28b9710c151eba75c2720
mgtburid <50088674+mgtburid@users.noreply.github.com>
2020-06-20 22:33:52 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 distro=$(cat /etc/os-release)
-mkdir gpg_files
+mkdir gpgim_files
 if [[ $distro == *"Red Hat"* || $distro == *"CentOs"*]]; then
 	echo -e "Apparently, you are running a Red Hat/CentOs box. The program will now need to utilize sudo privileges to do its job. Enter a password if prompted.\n"
 	sudo yum install gnupg2 openssl
